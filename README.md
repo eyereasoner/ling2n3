f7172ec05dba22a48d9dcc38aaffbabd4ef4887a
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2024-02-24 08:08:46 +0100

    Intial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Lingua to Notation3 translator
+
+## Synopsis
+
+```
+./ling2n3.sh examples/socrates.trig
+```
+
+## Description
+
+[Lingua](https://github.com/eyereasoner/eye/tree/master/lingua) is an experimental language to express reasoning using the [TriG](https://www.w3.org/TR/trig/) syntax.
+
+The `ling2n3.sh` is a script to use [Notation3](https://w3c.github.io/N3/spec/) to translate Lingua to Notation3. This only works with the [EYE](https://github.com/eyereasoner/eye/tree/master) 9.9.5 reasoner that as support for named quads.
+
+## Authors
+
+- Doerthe Arndt
+- Patrick Hochstenbach
---
+++ ling2n3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+FILE=$1 
+
+if [ "${FILE}" == "" ]; then
+    echo "$0 file"
+    exit 1
+fi
+
+eye --nope --quiet --quantify http://www.w3.org/2000/10/swap/var# --pass-only-new \
+    n3/aux.n3 n3/translation.n3 <(cat ${FILE} | sed -e 's/http:\/\/www.w3.org\/2000\/10\/swap\/lingua#/urn:lingua:/') > temp/t1.n3
+
+eye --nope --quiet --quantify http://www.w3.org/2000/10/swap/var#  --pass \
+    n3/data.n3 <(cat ${FILE} | sed -e 's/http:\/\/www.w3.org\/2000\/10\/swap\/lingua#/urn:lingua:/') > temp/t2.n3
+
+eye --nope --quiet --pass-all temp/t1.n3 temp/t2.n3
+
+rm temp/t1.n3 temp/t2.n3
